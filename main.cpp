ff08ae2e71aeb0f5f7efc7a7357742a10925a0c0
Forest-Lover <1771880185@qq.com>
2021-01-26 21:03:25 +0800

    change:补充射手辅助相关配置、增加英雄列表输出、优化输出格式

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <vector>
 #include <string>
 #include <map>
@@ -9,11 +10,21 @@
 using namespace std;
 
 enum JOB {
-    J_TOP   = 1,  //上单
-    J_JUN   = 2,  //打野
-    J_SUP   = 3,  //辅助
-    J_MID   = 4,  //中单
-    J_ADC   = 5,  //射手
+    J_INVALID   = 0,  //invalid
+    J_TOP       = 1,  //上单
+    J_JUN       = 2,  //打野
+    J_SUP       = 3,  //辅助
+    J_MID       = 4,  //中单
+    J_ADC       = 5,  //射手
+    J_SIZE
+};
+const string JOB_STR[J_SIZE] = {
+    "invalid_job",
+    "上单",
+    "打野",
+    "辅助",
+    "中单",
+    "射手",
 };
 
 
@@ -209,8 +220,69 @@ int main()
                             vector<pair<string, float> >{make_pair("guiguzi", 6.12), make_pair("simayi", 5.55), make_pair("yangyuhuan", 4.01)}, 
                             vector<pair<string, float> >{make_pair("milaidi", 6.06), make_pair("bianque", 5.12), make_pair("huangzhong", 4.01)}); 
     //-----------射手-------------
+    hero_map["luban"] 	= Hero("鲁班", J_ADC, 
+                            vector<pair<string, float> >{make_pair("dunshan", 8.52), make_pair("aguduo", 4.96), make_pair("taiyizhenren", 3.62)}, 
+                            vector<pair<string, float> >{make_pair("yingzheng", 3.83), make_pair("nvwa", 2.88), make_pair("lanlingwang", 2.38)}); 
+    hero_map["bailishouyue"] = Hero("百里守约", J_ADC, 
+                            vector<pair<string, float> >{make_pair("ake", 6.50), make_pair("xishi", 5.70), make_pair("lanlingwang", 5.56)}, 
+                            vector<pair<string, float> >{make_pair("change", 5.17), make_pair("milaidi", 4.87), make_pair("bianque", 3.50)}); 
+    hero_map["yuji"] 	= Hero("虞姬", J_ADC, 
+                            vector<pair<string, float> >{make_pair("sunwukong", 3.17), make_pair("guanyu", 2.74), make_pair("nakelulu", 2.59)}, 
+                            vector<pair<string, float> >{make_pair("aguduo", 3.99), make_pair("lubandashi", 3.67), make_pair("luna", 3.67)}); 
+    hero_map["houyi"] 	= Hero("后羿", J_ADC, 
+                            vector<pair<string, float> >{make_pair("yangyuhuan", 5.59), make_pair("machao", 4.94), make_pair("peiqinhu", 4.39)}, 
+                            vector<pair<string, float> >{make_pair("zhenji", 3.05), make_pair("lanlingwang", 2.99), make_pair("wangzhaojun", 2.34)}); 
+    hero_map["jialuo"] 	= Hero("伽罗", J_ADC, 
+                            vector<pair<string, float> >{make_pair("change", 16.39), make_pair("yadianna", 6.33), make_pair("liubei", 5.67)}, 
+                            vector<pair<string, float> >{make_pair("ake", 6.53), make_pair("lanlingwang", 6.45), make_pair("milaidi", 4.91)}); 
+    hero_map["direnjie"] = Hero("狄仁杰", J_ADC, 
+                            vector<pair<string, float> >{make_pair("bailixuance", 4.70), make_pair("liyuanfang", 4.45), make_pair("lubandashi", 4.14)}, 
+                            vector<pair<string, float> >{make_pair("jialuo", 3.47), make_pair("huangzhong", 2.74), make_pair("mengya", 2.59)}); 
+    hero_map["makeboluo"] = Hero("马可波罗", J_ADC, 
+                            vector<pair<string, float> >{make_pair("yingzheng", 6.38), make_pair("mengya", 5.46), make_pair("ganjiangmoye", 4.15)}, 
+                            vector<pair<string, float> >{make_pair("aguduo", 7.12), make_pair("milaidi", 6.74), make_pair("sikongzhen", 6.06)}); 
+    hero_map["sunshangxiang"] = Hero("孙尚香", J_ADC, 
+                            vector<pair<string, float> >{make_pair("lvbu", 2.43), make_pair("yingzheng", 2.24), make_pair("nvwa", 2.20)}, 
+                            vector<pair<string, float> >{make_pair("dunshan", 4.95), make_pair("milaidi", 2.80), make_pair("sikongzhen", 1.47)}); 
+    hero_map["liyuanfang"] = Hero("李元芳", J_ADC, 
+                            vector<pair<string, float> >{make_pair("ake", 6.82), make_pair("lanlingwa", 5.84), make_pair("guiguzi", 4.44)}, 
+                            vector<pair<string, float> >{make_pair("dunshan", 5.82), make_pair("direnjie", 4.45), make_pair("gongbenwuzang", 4.11)}); 
+    hero_map["gongsunli"]  = Hero("公孙离", J_ADC, 
+                            vector<pair<string, float> >{make_pair("shangguanwaner", 9.78), make_pair("nvwa", 8.85), make_pair("yingzheng", 6.49)}, 
+                            vector<pair<string, float> >{make_pair("milaidi", 7.01), make_pair("yase", 6.41), make_pair("zhangliang", 6.19)}); 
+    hero_map["huangzhong"] = Hero("黄忠", J_ADC, 
+                            vector<pair<string, float> >{make_pair("dunshan", 7.11), make_pair("zhangliang", 5.23), make_pair("yangyuhuan", 5.16)}, 
+                            vector<pair<string, float> >{make_pair("zhongkui", 6.02), make_pair("guiguzi", 3.37), make_pair("makeboluo", 3.26)}); 
+    hero_map["mengya"]  = Hero("蒙犽", J_ADC, 
+                            vector<pair<string, float> >{make_pair("aguduo", 8.13), make_pair("pangu", 6.52), make_pair("mengtian", 5.79)}, 
+                            vector<pair<string, float> >{make_pair("makeboluo", 5.46), make_pair("guanyu", 5.30), make_pair("luna", 4.84)}); 
+    hero_map["chengjisihan"] = Hero("成吉思汗", J_ADC, 
+                            vector<pair<string, float> >{make_pair("yingzheng", 3.22), make_pair("daqiao", 2.88), make_pair("luna", 2.76)}, 
+                            vector<pair<string, float> >{make_pair("lubandashi", 5.26), make_pair("milaidi", 4.43), make_pair("sunce", 4.07)}); 
+    hero_map["ailin"]   = Hero("艾琳", J_ADC, 
+                            vector<pair<string, float> >{}, 
+                            vector<pair<string, float> >{}); 
+    //-----------辅助-------------
+    hero_map["caiwenji"] = Hero("蔡文姬", J_SUP, 
+                            vector<pair<string, float> >{make_pair("zhugeliang", 3.93), make_pair("milaidi", 3.92), make_pair("daji", 3.00)}, 
+                            vector<pair<string, float> >{make_pair("dunshan", 8.61), make_pair("guiguzi", 7.14), make_pair("zhenji", 4.07)}); 
+    hero_map["yao"]     = Hero("瑶", J_SUP, 
+                            vector<pair<string, float> >{make_pair("wuzetian", 5.58), make_pair("nezha", 4.30), make_pair("nvwa", 3.62)}, 
+                            vector<pair<string, float> >{make_pair("jialuo", 5.18), make_pair("gongbenwuzang", 4.81), make_pair("dunshan", 4.05)}); 
+    hero_map["donghuangtaiyi"] = Hero("东皇太一", J_SUP, 
+                            vector<pair<string, float> >{make_pair("dunshan", 4.10), make_pair("lan", 3.15), make_pair("huamulan", 2.90)}, 
+                            vector<pair<string, float> >{make_pair("sulie", 3.45), make_pair("bailixuance", 3.14), make_pair("shenmengxi", 2.45)}); 
+    hero_map["dunshan"] = Hero("盾山", J_SUP, 
+                            vector<pair<string, float> >{make_pair("diaochan", 15.01), make_pair("shangguanwaner", 9.74), make_pair("caiwenji", 8.61)}, 
+                            vector<pair<string, float> >{make_pair("zhouyu", 13.63), make_pair("wangzhaojun", 12.61), make_pair("shenmengxi", 9.72)}); 
+    hero_map["mingshiyin"] = Hero("明世隐", J_SUP, 
+                            vector<pair<string, float> >{make_pair("nezha", 4.17), make_pair("chengjisihan", 3.61), make_pair("zhuangzhou", 3.39)}, 
+                            vector<pair<string, float> >{make_pair("mengtian", 4.83), make_pair("lubandashi", 4.68), make_pair("dunshan", 4.24)}); 
+    hero_map["zhuangzhou"] = Hero("庄周", J_SUP, 
+                            vector<pair<string, float> >{make_pair("sunce", 5.42), make_pair("niumo", 4.77), make_pair("wangzhaojun", 4.75)}, 
+                            vector<pair<string, float> >{make_pair("ganjiangmoye", 6.31), make_pair("diaochan", 5.56), make_pair("shangguanwaner", 5.46)}); 
     /*
-    hero_map[""] = Hero("", J_ADC, 
+    hero_map[""] = Hero("", J_SUP, 
                             vector<pair<string, float> >{make_pair("", ), make_pair("", ), make_pair("", )}, 
                             vector<pair<string, float> >{make_pair("", ), make_pair("", ), make_pair("", )}); 
     */
@@ -265,30 +337,52 @@ int main()
     //dump
     cout << "-----------dump restrain_map-----------" << endl;
     for (auto &kv : restrain_map) {
-        cout << "key:<" << kv.first.first << "," << kv.first.second << ">\tvalue:" << kv.second << endl;
+        stringstream ss_key;
+        ss_key << "key:<" << kv.first.first << "," << kv.first.second << ">";
+        cout << setiosflags(ios::left) << setw(30) << ss_key.str() << "\tvalue:" << kv.second << endl;
     }
     cout << "restrain_size:" << restrain_map.size() << endl;
+
+    cout << "-----------dump hero map-----------" << endl;
     cout << "英雄列表:" << endl;
-    int tmp_cnt = 0;
+    int tmp_cnt[J_SIZE] = {0};
+    stringstream ss[J_SIZE];
     for (auto &kv : hero_map) {
-        cout << kv.second._Chinese_name << "(" << kv.first << ")\t";
-        if (++tmp_cnt % 10 == 0)
-            cout << endl;
+        auto &hero = kv.second;
+        stringstream tmp_ss;
+        tmp_ss << hero._Chinese_name << "(" << kv.first << ")";
+        ss[hero._job] << setiosflags(ios::left) << setw(30) << tmp_ss.str() << "\t";
+        if (++tmp_cnt[hero._job] % 5 == 0)
+            ss[hero._job] << endl;
     }
-    cout << endl << "hero_size:" << restrain_map.size() << endl;
+    for (int i = 1; i < J_SIZE; ++i) {
+        cout << "<<" << JOB_STR[i] << ">>(" << tmp_cnt[i] << "):" << endl;
+        cout << ss[i].str() << endl << endl;;
+    }
+    cout << endl << "total hero size:" << hero_map.size() << endl;
+
 
     //开始选英雄
-    cout << "-----------CHOOSE HERO-----------" << endl;
-    enum {S_RED = 0, S_BLUE = 1};
+    enum SIDE {S_RED = 0, S_BLUE = 1, S_SIZE};
+    const string SIDE_STR[S_SIZE] = {
+        "红方",
+        "蓝方"
+    };
     const vector<int> SEQUENCE{1, 0, 0, 1, 1, 0, 0, 1, 1, 0};  //选将顺序 1:蓝方 2:红方
     vector<string> choosed_list;
-    for (auto cur_seq : SEQUENCE) {
+    for (int i = 0; i <= SEQUENCE.size(); ++i) {
+        int cur_side = S_SIZE;  //当前阵营 S_SIZE:观察统计结果
+        if (i < SEQUENCE.size())
+            cur_side = SEQUENCE[i];
         struct RestrainInfo {
             vector<float> _val_list;  //对每个英雄的克制值
-            float   _total_val      = 0.f;  //总净值
-            float   _abs_total_val  = 0.f;  //总绝对值
+            float   _total_val[S_SIZE]      = {0.f};  //总克制值
+            float   _abs_total_val[S_SIZE]  = {0.f};  //总绝对值
+            float   _enemy_total_val        = 0.f;  //敌对阵营总克制值
+            float   _aliance_total_val      = 0.f;  //友方阵营总克制值
         };
         map<string, RestrainInfo> restrain_info_map;  //所有英雄克制计算值
+        cout << "-----------CHOOSE HERO-----------" << endl;
         if (choosed_list.empty()) {
             //TODO 默认英雄
             cout << "默认推荐：。。。" << endl;
@@ -302,55 +396,90 @@ int main()
                 }
                 for (size_t idx = 0; idx < info._val_list.size() && idx < SEQUENCE.size(); ++idx) {
                     float val = info._val_list[idx];
-                    if (SEQUENCE[idx] == cur_seq) {
-                        //alliance
-                    } else {
-                        //enemy
-                        info._total_val += val;
-                        info._abs_total_val += fabs(val);
+                    info._total_val[SEQUENCE[idx]]      += val;
+                    info._abs_total_val[SEQUENCE[idx]]  += fabs(val);
+                }
+                if (cur_side != S_SIZE) {
+                    for (int i = 0; i < S_SIZE; ++i) {
+                        if (i == cur_side)
+                            info._aliance_total_val     += info._total_val[i];
+                        else
+                            info._enemy_total_val       += info._total_val[i];
                     }
                 }
             }
-            //sort
-            vector<pair<string, RestrainInfo>> info_list(restrain_info_map.begin(), restrain_info_map.end());
-            sort(info_list.begin(), info_list.end(), [](const decltype(info_list)::value_type &l, const decltype(info_list)::value_type &r){
-                 return l.second._total_val > r.second._total_val;
-                 });
-            //inform
-            const int RECOMMEND_COUNT = 5;  //推荐数量
-            int t_cnt = 0;
-            for (auto &kv : info_list) {
-                //skip choosed
-                auto it = find(choosed_list.begin(), choosed_list.end(), kv.first);
-                if (it != choosed_list.end())
+            //已选列表
+            cout << "---已选英雄---：" << endl;
+            stringstream ss[S_SIZE];
+            for (int i = 0; i < choosed_list.size() && i < SEQUENCE.size(); ++i) {
+                auto &name = choosed_list[i];
+                auto it_res     = restrain_info_map.find(name);
+                auto it_hero    = hero_map.find(name); 
+                if (it_res == restrain_info_map.end() || it_hero == hero_map.end())
                     continue;
-                //max show count
-                if (++t_cnt > RECOMMEND_COUNT)
-                    break;
-                auto &info_hero = hero_map[kv.first];
-                auto &info_res  = kv.second;
-                cout << "<" << t_cnt << ">" << info_hero._Chinese_name << ":\t";
-                for (size_t i = 0; i < info_res._val_list.size() && i < choosed_list.size() && i < SEQUENCE.size(); ++i) {
-                    //跳过己方
-                    if (cur_seq == SEQUENCE[i])
+                auto &res_info  = it_res->second; 
+                auto &hero_info = it_hero->second;
+                stringstream ss_tmp;
+                float enemy_total_val = 0.f;  //对敌方总克制值
+                for (int side = 0; side < S_SIZE; ++side) {
+                    if (side == SEQUENCE[i])
                         continue;
-                    cout << choosed_list[i] << ":" << info_res._val_list[i] << ",\t";
+                    enemy_total_val += res_info._total_val[side];
                 }
-                cout << "总克制值:" << info_res._total_val << "\t";
-                cout << "绝对值:" << info_res._abs_total_val << "\t";
-                cout << endl;
+                ss_tmp << hero_info._Chinese_name << "(" << enemy_total_val << ")";
+                ss[SEQUENCE[i]] << setiosflags(ios::left) << setw(30) << ss_tmp.str() << "\t";
+            }
+            for (int i = 0; i < S_SIZE; ++i) {
+                cout << "<" << SIDE_STR[i] << ">:" << ss[i].str() << endl;
+            }
+            //推荐英雄
+            if (cur_side != S_SIZE) {
+                cout << "---推荐英雄---：" << endl;
+                //sort
+                vector<pair<string, RestrainInfo>> info_list(restrain_info_map.begin(), restrain_info_map.end());
+                sort(info_list.begin(), info_list.end(), [](const decltype(info_list)::value_type &l, const decltype(info_list)::value_type &r){
+                    return l.second._enemy_total_val > r.second._enemy_total_val;
+                    });
+                //inform
+                const int RECOMMEND_COUNT = 5;  //推荐数量
+                int t_cnt = 0;
+                for (auto &kv : info_list) {
+                    //skip choosed
+                    auto it = find(choosed_list.begin(), choosed_list.end(), kv.first);
+                    if (it != choosed_list.end())
+                        continue;
+                    //max show count
+                    if (++t_cnt > RECOMMEND_COUNT)
+                        break;
+                    auto &info_hero = hero_map[kv.first];
+                    auto &info_res  = kv.second;
+                    cout << "<" << t_cnt << ">" << info_hero._Chinese_name << ":\t";
+                    for (size_t i = 0; i < info_res._val_list.size() && i < choosed_list.size() && i < SEQUENCE.size(); ++i) {
+                        //跳过己方
+                        if (cur_side == SEQUENCE[i])
+                            continue;
+                        cout << choosed_list[i] << ":" << info_res._val_list[i] << ",\t";
+                    }
+                    cout << "敌方:" << info_res._enemy_total_val << "\t";
+                    cout << "友方:" << info_res._aliance_total_val << "\t";
+                    cout << endl;
+                }
             }
         }
-        if (cur_seq == S_BLUE) {
-            cout << "<蓝方> 请选择英雄:";
-        } else if (cur_seq == S_RED) {
-            cout << "<红方> 请选择英雄:";
-        }
+
+        if (cur_side < S_SIZE)
+            cout << "<" << SIDE_STR[cur_side] << "> 请选择英雄：";
+        else 
+            cout << "英雄选择完毕！" << endl;
+
         string hero_name;
         while (cin >> hero_name) {
-            auto it = hero_map.find(hero_name);
-            if (it != hero_map.end())
+            auto it         = hero_map.find(hero_name);
+            auto it_choosed = find(choosed_list.begin(), choosed_list.end(), hero_name);
+            if (it != hero_map.end() && it_choosed == choosed_list.end()) {
+                //合法英雄 && 未被选择
                 break;
+            }
             cout << "无效值, 请重新输入:";
         }
         choosed_list.push_back(hero_name);
